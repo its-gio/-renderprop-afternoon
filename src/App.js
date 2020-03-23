3f352044a24275b34b37e65fd461207ecb983851
Giovani Aguirre <me@its-g.io>
2020-03-23 13:56:54 -0700

    Added increase and decrease functionality and added module to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import CurrencyConverter from './Components/CurrencyConverter';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <h2>Render Props</h2>
+        <CurrencyConverter render={() => {}} />
+      </>
     );
   }
 }
--- src/Components/CurrencyConverter.jsx
+++ src/Components/CurrencyConverter.jsx
@@ -15,23 +15,35 @@ export default class CurrencyConverter extends Component {
     amount: 0
   }
 
+  handleAmountIncrease = () => this.setState((prevState) => ({ amount: prevState.amount + 1 }));
+
+	handleAmountDecrease = () => this.state.amount > 0 && this.setState((prevState) => ({ amount: prevState.amount - 1 }));
+
+	handleOptionSelect = (event) => {
+		const userValue = event.target.value;
+		this.setState(() => ({ selectedCurrency: userValue, currencyChosen: userValue === 'Select Currency' ? false : true }));
+	}
+
   render() {
-    const currencyList = currencyData.map((currency, i) => <option key={i} value={i}>{currency.name}</option>)
+    const currencyList = currencyData.map((currency, i) => <option key={i} value={i}>{currency.name}</option>);
+
     return (
       <div>
-        <select name="" value={this.state.selectedCurrency}>
+        <select onChange={this.handleOptionSelect} value={this.state.selectedCurrency}>
           <option value='Select Currency'>Select Currency</option>
           {currencyList}
         </select>
 
-        <button>+</button>
+        <button onClick={this.handleAmountIncrease}>+</button>
 
-        <button>-</button>
+        <button onClick={this.handleAmountDecrease}>-</button>
 
-        {this.props.render(
-					currencyData[this.state.selectedCurrency],
-					this.state.amount
-				)}
+        {
+          this.props.render (
+            currencyData[this.state.selectedCurrency],
+            this.state.amount
+          )
+        }
       </div>
     )
   }
